c8aee231bd753fd961a98ce83876542ab88bc097
ganesh shinde <ganeshss978081@gmail.com>
2025-02-21 12:20:17 +0530

    initial commit

---
+++ app/src/main/java/com/gs/moneybook/Fragments/AddIEFragment.java
@@ -0,0 +1,27 @@
+package com.gs.moneybook.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gs.moneybook.R;
+
+public class AddIEFragment extends Fragment {
+
+
+    public AddIEFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_i_e, container, false);
+    }
+}
--- app/src/main/java/com/gs/moneybook/MainActivity.java
+++ app/src/main/java/com/gs/moneybook/MainActivity.java
@@ -1,13 +1,19 @@
 package com.gs.moneybook;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 //import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.gs.moneybook.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +25,35 @@ public class MainActivity extends AppCompatActivity {
        // EdgeToEdge.enable(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+
+                if(id == R.id.navDashboard){
+                    loadFragment();
+                } else if (id == R.id.navAdd) {
+
+                } else if (id == R.id.navProfile) {
+
+                }else{
+
+                }
+
+                return false;
+            }
+        });
+
+
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.containerMain,fragment);
+        ft.commit();
 
 
     }
